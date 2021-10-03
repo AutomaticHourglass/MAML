3b1c9cbd4a4016cc5fd44ae6c0a5c6a3be53fe19
Unsal Gokdag <unsal.gokdag@gmail.com>
2021-10-03 13:20:20 +0200

    Add model files

---
+++ model-segnet.py
@@ -0,0 +1,396 @@
+from keras.models import *
+from keras.layers import *
+from types import MethodType
+import random
+import six
+from keras.callbacks import Callback
+
+IMAGE_ORDERING_CHANNELS_LAST = "channels_last"
+IMAGE_ORDERING_CHANNELS_FIRST = "channels_first"
+
+# Default IMAGE_ORDERING = channels_last
+IMAGE_ORDERING = IMAGE_ORDERING_CHANNELS_LAST
+
+class_colors = [(random.randint(0, 255), random.randint(
+    0, 255), random.randint(0, 255)) for _ in range(5000)]
+
+def vanilla_encoder(input_height=224,  input_width=224):
+
+    kernel = 3
+    filter_size = 64
+    pad = 1
+    pool_size = 2
+
+    if IMAGE_ORDERING == 'channels_first':
+        img_input = Input(shape=(4, input_height, input_width))
+    elif IMAGE_ORDERING == 'channels_last':
+        img_input = Input(shape=(input_height, input_width, 4))
+
+    x = img_input
+    levels = []
+
+    x = (ZeroPadding2D((pad, pad), data_format=IMAGE_ORDERING))(x)
+    x = (Conv2D(filter_size, (kernel, kernel),
+                data_format=IMAGE_ORDERING, padding='valid'))(x)
+    x = (BatchNormalization())(x)
+    x = (Activation('relu'))(x)
+    x = (MaxPooling2D((pool_size, pool_size), data_format=IMAGE_ORDERING))(x)
+    levels.append(x)
+
+    x = (ZeroPadding2D((pad, pad), data_format=IMAGE_ORDERING))(x)
+    x = (Conv2D(128, (kernel, kernel), data_format=IMAGE_ORDERING,
+         padding='valid'))(x)
+    x = (BatchNormalization())(x)
+    x = (Activation('relu'))(x)
+    x = (MaxPooling2D((pool_size, pool_size), data_format=IMAGE_ORDERING))(x)
+    levels.append(x)
+
+    for _ in range(3):
+        x = (ZeroPadding2D((pad, pad), data_format=IMAGE_ORDERING))(x)
+        x = (Conv2D(256, (kernel, kernel),
+                    data_format=IMAGE_ORDERING, padding='valid'))(x)
+        x = (BatchNormalization())(x)
+        x = (Activation('relu'))(x)
+        x = (MaxPooling2D((pool_size, pool_size),
+             data_format=IMAGE_ORDERING))(x)
+        levels.append(x)
+
+    return img_input, levels
+
+def train(model,
+          train_images,
+          train_annotations,
+          input_height=None,
+          input_width=None,
+          n_classes=None,
+          verify_dataset=True,
+          checkpoints_path=None,
+          epochs=5,
+          batch_size=2,
+          validate=False,
+          val_images=None,
+          val_annotations=None,
+          val_batch_size=2,
+          auto_resume_checkpoint=False,
+          load_weights=None,
+          steps_per_epoch=512,
+          val_steps_per_epoch=512,
+          gen_use_multiprocessing=False,
+          ignore_zero_class=False,
+          optimizer_name='adam',
+          do_augment=False,
+          augmentation_name="aug_all"):
+
+#     from .models.all_models import model_from_name
+    # check if user gives model name instead of the model object
+    if isinstance(model, six.string_types):
+        # create the model from the name
+        assert (n_classes is not None), "Please provide the n_classes"
+        if (input_height is not None) and (input_width is not None):
+            model = model_from_name[model](
+                n_classes, input_height=input_height, input_width=input_width)
+        else:
+            model = model_from_name[model](n_classes)
+
+    n_classes = model.n_classes
+    input_height = model.input_height
+    input_width = model.input_width
+    output_height = model.output_height
+    output_width = model.output_width
+
+    if validate:
+        assert val_images is not None
+        assert val_annotations is not None
+
+    if optimizer_name is not None:
+
+        if ignore_zero_class:
+            loss_k = masked_categorical_crossentropy
+        else:
+            loss_k = 'categorical_crossentropy'
+
+        model.compile(loss=loss_k,
+                      optimizer=optimizer_name,
+                      metrics=['accuracy'])
+
+    if checkpoints_path is not None:
+        with open(checkpoints_path+"_config.json", "w") as f:
+            json.dump({
+                "model_class": model.model_name,
+                "n_classes": n_classes,
+                "input_height": input_height,
+                "input_width": input_width,
+                "output_height": output_height,
+                "output_width": output_width
+            }, f)
+
+    if load_weights is not None and len(load_weights) > 0:
+        print("Loading weights from ", load_weights)
+        model.load_weights(load_weights)
+
+    if auto_resume_checkpoint and (checkpoints_path is not None):
+        latest_checkpoint = find_latest_checkpoint(checkpoints_path)
+        if latest_checkpoint is not None:
+            print("Loading the weights from latest checkpoint ",
+                  latest_checkpoint)
+            model.load_weights(latest_checkpoint)
+
+    if verify_dataset:
+        print("Verifying training dataset")
+        verified = verify_segmentation_dataset(train_images,
+                                               train_annotations,
+                                               n_classes)
+        assert verified
+        if validate:
+            print("Verifying validation dataset")
+            verified = verify_segmentation_dataset(val_images,
+                                                   val_annotations,
+                                                   n_classes)
+            assert verified
+
+    train_gen = image_segmentation_generator(
+        train_images, train_annotations,  batch_size,  n_classes,
+        input_height, input_width, output_height, output_width,
+        do_augment=do_augment, augmentation_name=augmentation_name)
+
+    if validate:
+        val_gen = image_segmentation_generator(
+            val_images, val_annotations,  val_batch_size,
+            n_classes, input_height, input_width, output_height, output_width)
+
+    callbacks = [
+        CheckpointsCallback(checkpoints_path)
+    ]
+
+    if not validate:
+        model.fit_generator(train_gen, steps_per_epoch,
+                            epochs=epochs, callbacks=callbacks)
+    else:
+        model.fit_generator(train_gen,
+                            steps_per_epoch,
+                            validation_data=val_gen,
+                            validation_steps=val_steps_per_epoch,
+                            epochs=epochs, callbacks=callbacks,
+                            use_multiprocessing=gen_use_multiprocessing)
+
+def predict(model=None, inp=None, out_fname=None,
+            checkpoints_path=None, overlay_img=False,
+            class_names=None, show_legends=False, colors=class_colors,
+            prediction_width=None, prediction_height=None):
+
+    if model is None and (checkpoints_path is not None):
+        model = model_from_checkpoint_path(checkpoints_path)
+
+    assert (inp is not None)
+    assert ((type(inp) is np.ndarray) or isinstance(inp, six.string_types)),\
+        "Input should be the CV image or the input file name"
+
+    if isinstance(inp, six.string_types):
+        inp = cv2.imread(inp)
+
+    assert len(inp.shape) == 3, "Image should be h,w,3 "
+
+    output_width = model.output_width
+    output_height = model.output_height
+    input_width = model.input_width
+    input_height = model.input_height
+    n_classes = model.n_classes
+
+    x = get_image_array(inp, input_width, input_height,
+                        ordering=IMAGE_ORDERING)
+    pr = model.predict(np.array([x]))[0]
+    pr = pr.reshape((output_height,  output_width, n_classes)).argmax(axis=2)
+
+    seg_img = visualize_segmentation(pr, inp, n_classes=n_classes,
+                                     colors=colors, overlay_img=overlay_img,
+                                     show_legends=show_legends,
+                                     class_names=class_names,
+                                     prediction_width=prediction_width,
+                                     prediction_height=prediction_height)
+
+    if out_fname is not None:
+        cv2.imwrite(out_fname, seg_img)
+
+    return pr
+
+def predict_multiple(model=None, inps=None, inp_dir=None, out_dir=None,
+                     checkpoints_path=None, overlay_img=False,
+                     class_names=None, show_legends=False, colors=class_colors,
+                     prediction_width=None, prediction_height=None):
+
+    if model is None and (checkpoints_path is not None):
+        model = model_from_checkpoint_path(checkpoints_path)
+
+    if inps is None and (inp_dir is not None):
+        inps = glob.glob(os.path.join(inp_dir, "*.jpg")) + glob.glob(
+            os.path.join(inp_dir, "*.png")) + \
+            glob.glob(os.path.join(inp_dir, "*.jpeg"))
+        inps = sorted(inps)
+
+    assert type(inps) is list
+
+    all_prs = []
+
+    for i, inp in enumerate(tqdm(inps)):
+        if out_dir is None:
+            out_fname = None
+        else:
+            if isinstance(inp, six.string_types):
+                out_fname = os.path.join(out_dir, os.path.basename(inp))
+            else:
+                out_fname = os.path.join(out_dir, str(i) + ".jpg")
+
+        pr = predict(model, inp, out_fname,
+                     overlay_img=overlay_img, class_names=class_names,
+                     show_legends=show_legends, colors=colors,
+                     prediction_width=prediction_width,
+                     prediction_height=prediction_height)
+
+        all_prs.append(pr)
+
+    return all_prs
+
+def get_segmentation_model(input, output):
+
+    img_input = input
+    o = output
+
+    o_shape = Model(img_input, o).output_shape
+    i_shape = Model(img_input, o).input_shape
+
+    if IMAGE_ORDERING == 'channels_first':
+        output_height = o_shape[2]
+        output_width = o_shape[3]
+        input_height = i_shape[2]
+        input_width = i_shape[3]
+        n_classes = o_shape[1]
+#         o = (Reshape((-1, output_height*output_width)))(o)
+#         o = (Permute((2, 1)))(o)
+    elif IMAGE_ORDERING == 'channels_last':
+        output_height = o_shape[1]
+        output_width = o_shape[2]
+        input_height = i_shape[1]
+        input_width = i_shape[2]
+        n_classes = o_shape[3]
+#         o = (Reshape((output_height*output_width, -1)))(o)
+
+    o = (Activation('softmax'))(o)
+    model = Model(img_input, o)
+    model.output_width = output_width
+    model.output_height = output_height
+    model.n_classes = n_classes
+    model.input_height = input_height
+    model.input_width = input_width
+    model.model_name = ""
+
+    model.train = MethodType(train, model)
+    model.predict_segmentation = MethodType(predict, model)
+    model.predict_multiple = MethodType(predict_multiple, model)
+    model.evaluate_segmentation = MethodType(evaluate, model)
+
+    return model
+
+def segnet_decoder(f, n_classes, n_up=3):
+
+    assert n_up >= 2
+
+    o = f
+    o = (ZeroPadding2D((1, 1), data_format=IMAGE_ORDERING))(o)
+    o = (Conv2D(512, (3, 3), padding='valid', data_format=IMAGE_ORDERING))(o)
+    o = (BatchNormalization())(o)
+
+    o = (UpSampling2D((2, 2), data_format=IMAGE_ORDERING))(o)
+    o = (ZeroPadding2D((1, 1), data_format=IMAGE_ORDERING))(o)
+    o = (Conv2D(256, (3, 3), padding='valid', data_format=IMAGE_ORDERING))(o)
+    o = (BatchNormalization())(o)
+
+    for _ in range(n_up-2):
+        o = (UpSampling2D((2, 2), data_format=IMAGE_ORDERING))(o)
+        o = (ZeroPadding2D((1, 1), data_format=IMAGE_ORDERING))(o)
+        o = (Conv2D(128, (3, 3), padding='valid',
+             data_format=IMAGE_ORDERING))(o)
+        o = (BatchNormalization())(o)
+
+    o = (UpSampling2D((2, 2), data_format=IMAGE_ORDERING))(o)
+    o = (ZeroPadding2D((1, 1), data_format=IMAGE_ORDERING))(o)
+    o = (Conv2D(64, (3, 3), padding='valid', data_format=IMAGE_ORDERING))(o)
+    o = (BatchNormalization())(o)
+
+    o = Conv2D(n_classes, (3, 3), padding='same',
+               data_format=IMAGE_ORDERING)(o)
+
+    return o
+
+def _segnet(n_classes, encoder,  input_height=416, input_width=608,
+            encoder_level=3):
+
+    img_input, levels = encoder(
+        input_height=input_height,  input_width=input_width)
+
+    feat = levels[encoder_level]
+    o = segnet_decoder(feat, n_classes, n_up=3)
+    model = get_segmentation_model(img_input, o)
+
+    return model
+
+
+def segnet(n_classes, input_height=416, input_width=608, encoder_level=3):
+
+    model = _segnet(n_classes, vanilla_encoder,  input_height=input_height,
+                    input_width=input_width, encoder_level=encoder_level)
+    model.model_name = "segnet"
+    return model
+
+def evaluate(model=None, inp_images=None, annotations=None,
+             inp_images_dir=None, annotations_dir=None, checkpoints_path=None):
+
+    if model is None:
+        assert (checkpoints_path is not None),\
+                "Please provide the model or the checkpoints_path"
+        model = model_from_checkpoint_path(checkpoints_path)
+
+    if inp_images is None:
+        assert (inp_images_dir is not None),\
+                "Please provide inp_images or inp_images_dir"
+        assert (annotations_dir is not None),\
+            "Please provide inp_images or inp_images_dir"
+
+        paths = get_pairs_from_paths(inp_images_dir, annotations_dir)
+        paths = list(zip(*paths))
+        inp_images = list(paths[0])
+        annotations = list(paths[1])
+
+    assert type(inp_images) is list
+    assert type(annotations) is list
+
+    tp = np.zeros(model.n_classes)
+    fp = np.zeros(model.n_classes)
+    fn = np.zeros(model.n_classes)
+    n_pixels = np.zeros(model.n_classes)
+
+    for inp, ann in tqdm(zip(inp_images, annotations)):
+        pr = predict(model, inp)
+        gt = get_segmentation_array(ann, model.n_classes,
+                                    model.output_width, model.output_height,
+                                    no_reshape=True)
+        gt = gt.argmax(-1)
+        pr = pr.flatten()
+        gt = gt.flatten()
+
+        for cl_i in range(model.n_classes):
+
+            tp[cl_i] += np.sum((pr == cl_i) * (gt == cl_i))
+            fp[cl_i] += np.sum((pr == cl_i) * ((gt != cl_i)))
+            fn[cl_i] += np.sum((pr != cl_i) * ((gt == cl_i)))
+            n_pixels[cl_i] += np.sum(gt == cl_i)
+
+    cl_wise_score = tp / (tp + fp + fn + 0.000000000001)
+    n_pixels_norm = n_pixels / np.sum(n_pixels)
+    frequency_weighted_IU = np.sum(cl_wise_score*n_pixels_norm)
+    mean_IU = np.mean(cl_wise_score)
+
+    return {
+        "frequency_weighted_IU": frequency_weighted_IU,
+        "mean_IU": mean_IU,
+        "class_wise_IU": cl_wise_score
+    }
