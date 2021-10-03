c2ef7692bf9823aa9991b0e220a4ec85d0bf7c03
Unsal Gokdag <unsal.gokdag@gmail.com>
2021-10-03 15:57:08 +0200

    Standardize image_shape param

--- model_danet.py
+++ model_danet.py
@@ -156,6 +156,7 @@ def bottleneck_Block(input, out_filters, strides=(1, 1), dilation=(1, 1), with_c
 
 def danet_resnet101(input_shape, num_classes):
     input = Input(shape=tuple(input_shape))
+    print(input)
 
     conv1_1 = Conv2D(64, 7, strides=(2, 2), padding='same', use_bias=False, kernel_initializer='he_normal')(input)
     conv1_1 = BatchNormalization(axis=3)(conv1_1)
