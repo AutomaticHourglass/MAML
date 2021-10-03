a16fd336215a3dca1c745d467e42eed8eb90f8c9
Unsal Gokdag <unsal.gokdag@gmail.com>
2021-10-03 14:03:50 +0200

    Add model constructor

--- model.py
+++ model.py
@@ -2,10 +2,10 @@
 
 import model_segnet
 import model_danet
+from keras_unet.models.custom_unet import custom_unet
 
 class SSegModel():
     def __init__(self,model_name,model_params):
         if(model_name == 'unet'):
-            
-        pass
-
+            model = custom_unet(**model_params)
+        return model
