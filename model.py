52d843aaef75626acd7ff8051b5e6dacb17514fb
Unsal Gokdag <unsal.gokdag@gmail.com>
2021-10-03 14:20:26 +0200

    Add model constructor

--- model.py
+++ model.py
@@ -8,6 +8,8 @@ class SSegModel:
     def __init__(self,model_name,model_params):
         if(model_name == 'unet'):
             self.model = custom_unet(**model_params)
+        else:
+            self.model = None
 
     def hello(self):
         pass
