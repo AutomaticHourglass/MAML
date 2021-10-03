c1b4be2cc8b97d4ec71029bb89c9b6d439dba102
Unsal Gokdag <unsal.gokdag@gmail.com>
2021-10-03 14:30:21 +0200

    Add model constructor

--- model.py
+++ model.py
@@ -1,15 +1,17 @@
 # General collecting part of all the models
 
-# import model_segnet
+import model_segnet
 # import model_danet
 from .keras_unet.models.custom_unet import custom_unet
 
 class SSegModel:
     def __init__(self,model_name,model_params):
+        self.model_name = model_name
+        self.model_params = model_params
+
         if(model_name == 'unet'):
             self.model = custom_unet(**model_params)
+        elif(model_name) == 'segnet':
+            self.model = model_segnet.segnet(**model_params)
         else:
             self.model = None
-
-    def hello(self):
-        pass
