d36e6745f997c5cda75859e9cb84846cbe3b7e6b
Unsal Gokdag <unsal.gokdag@gmail.com>
2021-10-03 14:25:00 +0200

    Add model constructor

--- model.py
+++ model.py
@@ -5,12 +5,11 @@
 from .keras_unet.models.custom_unet import custom_unet
 
 class SSegModel:
-    pass
-    # def __init__(self,model_name,model_params):
-    #     if(model_name == 'unet'):
-    #         self.model = custom_unet(**model_params)
-    #     else:
-    #         self.model = None
+    def __init__(self,model_name,model_params):
+        if(model_name == 'unet'):
+            self.model = custom_unet(**model_params)
+        else:
+            self.model = None
 
-    # def hello(self):
-    #     pass
+    def hello(self):
+        pass
