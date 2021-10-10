0ab5dae3d7551332ef8b1d232086a54ac5ec70ce
Unsal Gokdag <unsal.gokdag@gmail.com>
2021-10-10 12:38:52 +0200

    Change imports to fix library

--- keras_unet_collection/_model_transunet_2d.py
+++ keras_unet_collection/_model_transunet_2d.py
@@ -270,7 +270,7 @@ def transunet_2d_base(input_tensor, filter_num, stack_num_down=2, stack_num_up=2
             
     return X
 
-def transunet_2d(input_shape, filter_num, n_labels, stack_num_down=2, stack_num_up=2,
+def transunet_2d(input_shape, filter_num, num_classes, stack_num_down=2, stack_num_up=2,
                  embed_dim=768, num_mlp = 3072, num_heads=12, num_transformer=12,
                  activation='ReLU', mlp_activation='GELU', output_activation='Softmax', batch_norm=False, pool=True, unpool=True, 
                  backbone=None, weights='imagenet', freeze_backbone=True, freeze_batch_norm=True, name='transunet'):
@@ -288,7 +288,7 @@ def transunet_2d(input_shape, filter_num, n_labels, stack_num_down=2, stack_num_
         filter_num: a list that defines the number of filters for each \
                     down- and upsampling levels. e.g., `[64, 128, 256, 512]`.
                     The depth is expected as `len(filter_num)`.
-        n_labels: number of output labels.
+        num_classes: number of output labels.
         stack_num_down: number of convolutional layers per downsampling level/block. 
         stack_num_up: number of convolutional layers (after concatenation) per upsampling level/block.
         activation: one of the `tensorflow.keras.layers` or `keras_unet_collection.activations` interfaces, e.g., 'ReLU'.
@@ -342,7 +342,7 @@ def transunet_2d(input_shape, filter_num, n_labels, stack_num_down=2, stack_num_
                           backbone=backbone, weights=weights, freeze_backbone=freeze_backbone, freeze_batch_norm=freeze_batch_norm, name=name)
     
     # output layer
-    OUT = CONV_output(X, n_labels, kernel_size=1, activation=output_activation, name='{}_output'.format(name))
+    OUT = CONV_output(X, num_classes, kernel_size=1, activation=output_activation, name='{}_output'.format(name))
     
     # functional API model
     model = Model(inputs=[IN,], outputs=[OUT,], name='{}_model'.format(name))
