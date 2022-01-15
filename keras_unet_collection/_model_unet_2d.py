0aa8afe1965c48a2e0a1a433ab84890f2cd9571e
Unsal Gokdag <unsal.gokdag@gmail.com>
2022-01-15 15:25:35 +0100

    Add CRF

--- keras_unet_collection/_model_unet_2d.py
+++ keras_unet_collection/_model_unet_2d.py
@@ -297,3 +297,79 @@ def unet_2d(input_size, filter_num, num_classes, stack_num_down=2, stack_num_up=
     model = Model(inputs=[IN,], outputs=[OUT,], name='{}_model'.format(name))
     
     return model
+
+def unet_2d_crf(input_size, filter_num, num_classes, stack_num_down=2, stack_num_up=2,
+            activation='ReLU', output_activation='Softmax', batch_norm=False, pool=True, unpool=True, 
+            backbone=None, weights='imagenet', freeze_backbone=True, freeze_batch_norm=True, name='unet'):
+    '''
+    U-net with an optional ImageNet-trained bakcbone. with CRF addition
+    
+    unet_2d(input_size, filter_num, num_classes, stack_num_down=2, stack_num_up=2,
+            activation='ReLU', output_activation='Softmax', batch_norm=False, pool=True, unpool=True, 
+            backbone=None, weights='imagenet', freeze_backbone=True, freeze_batch_norm=True, name='unet')
+    
+    ----------
+    Ronneberger, O., Fischer, P. and Brox, T., 2015, October. U-net: Convolutional networks for biomedical image segmentation. 
+    In International Conference on Medical image computing and computer-assisted intervention (pp. 234-241). Springer, Cham.
+    
+    Input
+    ----------
+        input_size: the size/shape of network input, e.g., `(128, 128, 3)`.
+        filter_num: a list that defines the number of filters for each \
+                    down- and upsampling levels. e.g., `[64, 128, 256, 512]`.
+                    The depth is expected as `len(filter_num)`.
+        num_classes: number of output labels.
+        stack_num_down: number of convolutional layers per downsampling level/block. 
+        stack_num_up: number of convolutional layers (after concatenation) per upsampling level/block.
+        activation: one of the `tensorflow.keras.layers` or `keras_unet_collection.activations` interfaces, e.g., 'ReLU'.
+        output_activation: one of the `tensorflow.keras.layers` or `keras_unet_collection.activations` interface or 'Sigmoid'.
+                           Default option is 'Softmax'.
+                           if None is received, then linear activation is applied.
+        batch_norm: True for batch normalization.
+        pool: True or 'max' for MaxPooling2D.
+              'ave' for AveragePooling2D.
+              False for strided conv + batch norm + activation.
+        unpool: True or 'bilinear' for Upsampling2D with bilinear interpolation.
+                'nearest' for Upsampling2D with nearest interpolation.
+                False for Conv2DTranspose + batch norm + activation.                 
+        name: prefix of the created keras model and its layers.
+        
+        ---------- (keywords of backbone options) ----------
+        backbone_name: the bakcbone model name. Should be one of the `tensorflow.keras.applications` class.
+                       None (default) means no backbone. 
+                       Currently supported backbones are:
+                       (1) VGG16, VGG19
+                       (2) ResNet50, ResNet101, ResNet152
+                       (3) ResNet50V2, ResNet101V2, ResNet152V2
+                       (4) DenseNet121, DenseNet169, DenseNet201
+                       (5) EfficientNetB[0-7]
+        weights: one of None (random initialization), 'imagenet' (pre-training on ImageNet), 
+                 or the path to the weights file to be loaded.
+        freeze_backbone: True for a frozen backbone.
+        freeze_batch_norm: False for not freezing batch normalization layers.
+        
+    Output
+    ----------
+        model: a keras model.
+    
+    '''
+    activation_func = eval(activation)
+    
+    if backbone is not None:
+        bach_norm_checker(backbone, batch_norm)
+        
+    IN = Input(input_size)
+    
+    # base    
+    X = unet_2d_base(IN, filter_num, stack_num_down=stack_num_down, stack_num_up=stack_num_up, 
+                     activation=activation, batch_norm=batch_norm, pool=pool, unpool=unpool, 
+                     backbone=backbone, weights=weights, freeze_backbone=freeze_backbone, 
+                     freeze_batch_norm=freeze_backbone, name=name)
+    
+    # output layer
+    OUT = CONV_output(X, num_classes, kernel_size=1, activation=output_activation, name='{}_output'.format(name))
+    
+    # functional API model
+    model = Model(inputs=[IN,], outputs=[OUT,], name='{}_model'.format(name))
+    
+    return model
