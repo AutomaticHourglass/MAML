c63a0d3894f5cbfd769bb9f61f2224e97ba8aa88
Unsal Gokdag <unsal.gokdag@gmail.com>
2022-01-15 16:13:37 +0100

    Add CRF Layer to Unet

--- keras_unet_collection/_model_unet_2d.py
+++ keras_unet_collection/_model_unet_2d.py
@@ -367,9 +367,9 @@ def unet_2d_crf(input_size, filter_num, num_classes, stack_num_down=2, stack_num
                      activation=activation, batch_norm=batch_norm, pool=pool, unpool=unpool, 
                      backbone=backbone, weights=weights, freeze_backbone=freeze_backbone, 
                      freeze_batch_norm=freeze_backbone, name=name)
-    X = CRF()(X)
+    OUT = CRF()(X)
     # output layer
-    OUT = CONV_output(X, num_classes, kernel_size=1, activation=output_activation, name='{}_output'.format(name))
+    # OUT = CONV_output(X, num_classes, kernel_size=1, activation=output_activation, name='{}_output'.format(name))
     
     # functional API model
     model = Model(inputs=[IN,], outputs=[OUT,], name='{}_model'.format(name))
